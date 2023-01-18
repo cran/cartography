249920147746611242a9815fe2d4e004756555f2
Timothée Giraud <timothee.giraud@cnrs.fr>
2023-01-18 12:00:02 +0000

    version 3.1.3

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,22 @@
+# cartography 3.1.3
+
+## Minor change
+- deactivate examples for get*Borders() functions
+
+
+
+# cartography 3.1.2
+
+## Minor change
+- lighter examples for get*Borders() functions
+
+
+# cartography 3.1.0
+## Major changes
+- defunct getTiles(), please use maptiles package instead
+- remove rgeos, rgdal and slippymath from Imports
+- deprecate smoothLayer(), please use potential package instead
+
 # cartography 3.0.1
 ## Major changes
 - Remove defunct arguments in dotdensityLayer, getBorders, getFigDim, 
--- inst/doc/cartography.R
+++ inst/doc/cartography.R
@@ -262,43 +262,6 @@ layoutLayer(title = "Commuting to Prefectures in Martinique",
             frame = FALSE, col = "grey25", coltitle = "white",
             tabtitle = TRUE)
 
-## ----isopleth, fig.width = 5--------------------------------------------------
-library(sf)
-library(cartography)
-# path to the geopackage file embedded in cartography
-path_to_gpkg <- system.file("gpkg/mtq.gpkg", package="cartography")
-# import to an sf object
-mtq <- st_read(dsn = path_to_gpkg, quiet = TRUE)
-# plot municipalities (only the backgroung color is plotted)
-plot(st_geometry(mtq), col = NA, border = NA, bg = "lightblue1")
-# plot isopleth map
-smoothLayer(
-  x = mtq, 
-  var = 'POP',
-  typefct = "exponential",
-  span = 4000,
-  beta = 2,
-  nclass = 12,
-  col = carto.pal(pal1 = 'brown.pal', n1 = 12),
-  border = "grey",
-  lwd = 0.1, 
-  mask = mtq, 
-  legend.values.rnd = -3,
-  legend.title.txt = "Population\nPotential",
-  legend.pos = "topright", 
-  add=TRUE
-)
-# annotation on the map
-text(x = 692582, y = 1611478, cex = 0.8, adj = 0, font = 3,  labels = 
-       "Distance function:\n- type = exponential\n- beta = 2\n- span = 4 km")
-# layout
-layoutLayer(title = "Population Distribution in Martinique",
-            sources = "Sources: Insee and IGN, 2018",
-            author = paste0("cartography ", packageVersion("cartography")),
-            frame = FALSE, north = FALSE, tabtitle = TRUE, theme = "brown.pal")
-# north arrow
-north(pos = "topleft")
-
 ## ----grid---------------------------------------------------------------------
 library(sf)
 library(cartography)
